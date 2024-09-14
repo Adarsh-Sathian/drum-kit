f9cdbea058e06e64088376d657864e910d9094c7
Adarsh-Sathian <adarshsathian1@gmail.com>
2024-09-14 11:26:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+var numDrumBtn = document.querySelectorAll(".drum").length;
+
+
+for(var i = 0; i < numDrumBtn; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var btnInnerHTML = this.innerHTML;
+        
+        makeSound(btnInnerHTML);
+
+        btnAnimation(btnInnerHTML);
+
+    });
+}
+
+
+// var audio = new Audio("./sounds/tom-1.mp3");
+//         audio.play();
+
+
+document.addEventListener("keydown", function (event) {
+    var keyBtn = event.key;
+    makeSound(keyBtn);
+    btnAnimation(keyBtn);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;  
+        case "d":
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break; 
+        case "l":
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            console.log(keyBtn);
+            break;
+    }
+}
+
+function btnAnimation(currentKey) {
+    var activeBtn = document.querySelector("." + currentKey);
+    activeBtn.classList.toggle("pressed");
+    setTimeout(function () {
+        activeBtn.classList.toggle("pressed");
+    }, 100);
+}
